5cf117f4f599bb1927eaeae34400d9d95dc4ab91
Madhubhashana Mahindarathne <104136158+Madhubhashana99@users.noreply.github.com>
2024-04-16 21:10:18 +0530

    update

--- forms/contact.php
+++ forms/contact.php
@@ -15,6 +15,8 @@
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
 
+  ?>
+
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   /*
   $contact->smtp = array(
